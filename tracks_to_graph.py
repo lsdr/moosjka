a7e1801fcf6f4282b16824c5b8e9b85dc4129b88
Luiz Rocha <lsdrocha@gmail.com>
2014-08-15 15:24:15 -0300

    registering songs in neo4j, preparing to import the whole db

--- tracks_to_graph.py
+++ tracks_to_graph.py
@@ -8,6 +8,12 @@ __license__ = 'public domain'
 
 from bulbs.model import Node, Relationship
 from bulbs.property import String, DateTime
+from bulbs.neo4jserver import Graph
+from datetime import datetime
+from copy import copy
+
+import cPickle as pickle
+
 
 class Song(Node):
     element_type = 'song'
@@ -23,12 +29,56 @@ class Followed(Relationship):
     ts = DateTime(nullable=False)
 
 
+if __name__ == '__main__':
+    g = Graph()
+    g.add_proxy('song', Song)
+    g.add_proxy(')ollowed', Followed)
 
-# from bulbs.neo4jserver import Graph
-#
-# g = Graph()
-# g.add_proxy('song', Song)
-# g.add_proxy('followed', Followed)
+    '''
+    as datas do track sao o timestamp em que a musica terminou de ser
+    ouvida (e de-facto scrobblada), entao a musica que SEGUIU a musica
+    o fez no ts da musica que ANTECEDEU
+    
+    os arquivos estao em ordem cronologica reversa, ou seja, no track-00001
+    a primeira posicao foi a ultima musica capturada (db[0]), a segunda
+    posicao (db[1]) foi a musica que ANTECEDEU a db[0].
+
+    entao:
+    song_depois = g.song.get_or_create(name=db[0].name, ...)
+    song_antes  = g.song.get_or_create(name=db[1].name, ...)
+    g.followed.create(db[0], db[1], ts=db[1].ts)
+    '''
+    db = pickle.load(open('db/tracks-00001.db'))
+    
+    song_antes  = g.song.get_or_create('name',
+                                       db[1][1],
+                                       name=db[1][1],
+                                       artist=db[1][0],
+                                       url=db[1][2])
+
+    song_depois = g.song.get_or_create('name', 
+                                       db[0][1],
+                                       name=db[0][1], 
+                                       artist=db[0][0],
+                                       url=db[0][2])
+
+    ts = datetime.fromtimestamp(int(db[1][3]))
+    g.followed.create(song_depois, song_antes, ts=ts)
+ 
+    # def zip_db(db, prev=())
+    #     _tmp_db = copy(db)
+    #     _tmp_db.insert(0, prev)
+    #     _tmp_db.pop()
+    #     return zip(db, _tmp_db)
+    #
+    # prev=()
+    # db_files = glob()
+    # for db_file in db_files:
+    #     for antes, depois in zip_db(db, prev):
+    #         if depois:
+    #             add_to_graph(antes, depois)
+    #         prev=antes
+
 #
 # song_1 = g.song.get_or_create(name='Crackerman', artist='STP')
 # song_2 = g.song.get_or_create(name='Dump', artist='Nirvana')
